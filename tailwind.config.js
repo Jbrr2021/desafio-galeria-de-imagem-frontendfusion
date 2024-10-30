e69b20df31de853fe91795135d74315c39fc591a
João Batista Rodrigues Ribeiro <87319898+Jbrr2021@users.noreply.github.com>
2024-10-30 19:35:14 -0300

    Adiciona configurações do Tailwind e postcss, remove SVG desnecessário

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Verifica o arquivo HTML na raiz do projeto
+    "./src/**/*.{js,ts,jsx,tsx}", // Verifica arquivos JavaScript e TypeScript dentro da pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
