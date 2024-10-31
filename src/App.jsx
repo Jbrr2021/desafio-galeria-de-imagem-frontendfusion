b6d1ccc0fb72f7c8462eceacb9a5f5b1a52e49d7
João Batista Rodrigues Ribeiro <87319898+Jbrr2021@users.noreply.github.com>
2024-10-30 23:42:58 -0300

    Configurando o HTML e App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
-// App.jsx
-import React, { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import ImageCards from './Components/ImageCards'; 
-import ImageCardDetails from './Components/ImageCardDetails';
-import Favorites from './Components/Favorites';
-import fetchData from './util/fetchdata'; // Certifique-se de que o caminho está correto
+import React from 'react';
+import Galeria from './Components/Galeria';
 
 function App() {
-  const [photos, setPhotos] = useState([]);
-  const [favorites, setFavorites] = useState([]);
-
-  useEffect(() => {
-    fetchData(setPhotos, 1); // Carrega a primeira página
-  }, []);
-
-  const toggleFavorite = (id) => {
-    if (favorites.includes(id)) {
-      setFavorites(favorites.filter(fav => fav !== id)); // Remove dos favoritos
-    } else {
-      setFavorites([...favorites, id]); // Adiciona aos favoritos
-    }
-  };
-
   return (
-    <Routes>
-        <Route path="/" element={<ImageCards heading="Galeria de Imagens" favorites={favorites} toggleFavorite={toggleFavorite} />} />
-        <Route path="/detailPage/:id" element={<ImageCardDetails />} />
-        <Route path="/favorites" element={<Favorites favorites={favorites} allPhotos={photos} />} />
-    </Routes>
+    <div className="bg-black min-h-screen text-white">
+      <div className="container mx-auto p-4">
+        <h1 className="bg-black text-white text-center py-16 text-3xl font-bold mb-4">Galeria de imagens API</h1>
+        <ImageGallery />
+      </div>
+    </div>
   );
 }
 
